7c81a01fbfaae39bb5223ae0328f327f157c3e79
logged73 <logged73@kingshigh.school.nz>
2025-05-26 12:25:45 +1200

    added fog and more lighting

--- urbanpursuit/Advanced Vehicle Controller/Scenes/menu.gd
+++ urbanpursuit/Advanced Vehicle Controller/Scenes/menu.gd
@@ -9,3 +9,9 @@ func _on_new_game_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+#func _ready() ->void:
+#func _physics_process(delta: float) -> void:
+		#const move_speed :=4.0
+		#%PathFollow3D.progress += move_speed * delta
